aa4bc0616c8de7eb7db69be96f89517aae6fc2c9
Ktoto660 <68076424+Ktoto660@users.noreply.github.com>
2020-07-16 23:17:10 +0300

    Update TaskLib_H.h

--- TaskLib_H.h
+++ TaskLib_H.h
@@ -1,15 +1,21 @@
 #pragma once
 
-
 #ifdef Task_Lib
 #define TASKLIBRARY_API __declspec(dllexport)
 #else
 #define TASKLIBRARY_API __declspec(dllimport)
 #endif
+////////////////////////////////////
+
+// В хедере объявляется каждая функция, объект и тип данных, являющиеся частью интерфейса вызова модуля  (*.h файле) 
+// Грубо говоря, в заголовочном файле ты пишешь шаблон, прототип своей программы, а реализация классов и методов уже делается в отдельном *.cpp файле
+
 
 using namespace std;
+#include <iostream>
 #include <string>
 
+// Класс для статуса задания
 class TaskState
 {
 private:
@@ -19,13 +25,14 @@ public:
 	TaskState();
 	TaskState(string a_state);
 
-	string getState();
+	string getState(); 
 };
 
+//Класс для задания
 class Task
 {
 private:
-	string name;
+	string name; 
 	string description;
 	TaskState state;
 	string startDate;
